2eec1542d8867677d4e0cd367ad7e7f3e3533673
Deborah Kurz <DeborahKurz6@gmail.com>
2024-10-14 12:42:33 -0600

    User and Recipe model's attributes created

--- server/models.py
+++ server/models.py
@@ -6,10 +6,22 @@ from config import db, bcrypt
 
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
+    id = db.Column(db.String, unique=True, nullable=False)
+    username = db.Column(db.String)
+    _password_hash = db.Column(db.String)
+    image_url = db.Column(db.String)
+    bio = db.Column(db.String)
 
+    recipes = db.relationship('Recipe', back_populates='user', cascade='all, delete-orphan')
     pass
 
 class Recipe(db.Model, SerializerMixin):
     __tablename__ = 'recipes'
-    
+    id = db.Column(db.String, unique=True, nullable=False)
+    title = db.Column(db.String)
+    instructions = db.Column(db.String)
+    minutes_to_complete = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    user = db.relationship('User', back_populates='recipes')
     pass
