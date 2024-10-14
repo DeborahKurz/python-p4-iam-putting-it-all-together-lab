ca3a14ffd184a0606926452be8bb6796c224d825
Deborah Kurz <DeborahKurz6@gmail.com>
2024-10-14 12:51:44 -0600

    User model constraints implimented

--- server/models.py
+++ server/models.py
@@ -6,22 +6,23 @@ from config import db, bcrypt
 
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
-    id = db.Column(db.String, unique=True, nullable=False)
-    username = db.Column(db.String)
-    _password_hash = db.Column(db.String)
+
+    id = db.Column(db.Integer, primary_key=True, unique=True, nullable=False)
+    username = db.Column(db.String, unique=True, nullable=False)
+    _password_hash = db.Column(db.String, nullable=False)
     image_url = db.Column(db.String)
     bio = db.Column(db.String)
 
     recipes = db.relationship('Recipe', back_populates='user', cascade='all, delete-orphan')
-    pass
+    
 
 class Recipe(db.Model, SerializerMixin):
     __tablename__ = 'recipes'
-    id = db.Column(db.String, unique=True, nullable=False)
-    title = db.Column(db.String)
+
+    id = db.Column(db.Integer, primary_key=True, unique=True, nullable=False)
+    title = db.Column(db.String, nullable=False)
     instructions = db.Column(db.String)
     minutes_to_complete = db.Column(db.Integer)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
     user = db.relationship('User', back_populates='recipes')
-    pass
